25793bed800d1d48f6059f421fd6d0f03ecb2554
sahith <bandasahithreddy@gmail.com>
2021-05-04 16:14:23 +0530

    apple added

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ app.get("/", function(req, res){
 })
 
 
+
 app.use(express.static(__dirname+"/frontend"));
+
+
+
 
 
 app.get("/google",function(req,res)
@@ -20,6 +24,12 @@ app.get("/resume",function(req,res)
     res.sendFile(__dirname+"/frontend/html/resume.html");
 });
  
+
+
+app.get("/apple",function(req,res)
+{
+    res.sendFile(__dirname+"/frontend/html/apple.html");
+})
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
  
